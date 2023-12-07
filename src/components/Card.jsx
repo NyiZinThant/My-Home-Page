4a5640dddeb53c970c5604b8ca3eaf71d74cee32
Nyi Zin Thant <nyizinthant@gmail.com>
2023-12-07 09:50:03 +0630

    fix missing plus icon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,11 @@ export default function Card({ item, toggleModal }) {
       className="border-2 rounded-2xl backdrop:blur-lg flex justify-center items-center bg-gray-900 hover:bg-gray-600 active:bg-gray-700 duration-100 overflow-hidden w-[4.5rem] h-[4.5rem]"
       onClick={toggleModal}
     >
-      {item.image && <img src={item.image} alt={item.name} className="w-11" />}
+      {item.image ? (
+        <img src={item.image} alt={item.name} className="w-11" />
+      ) : (
+        <FontAwesomeIcon icon={item.icon} className="text-3xl text-white" />
+      )}
     </button>
   );
 }
