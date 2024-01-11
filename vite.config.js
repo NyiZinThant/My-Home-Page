ce1b54afd822023f2dfd7bba30f59b719041dc7f
Nyi Zin Thant <nyizinthant@gmail.com>
2024-01-11 16:00:59 +0630

    add crxjs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import macrosPlugin from 'vite-plugin-babel-macros';
+import manifest from './manifest.json';
+import { crx } from '@crxjs/vite-plugin';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), macrosPlugin()],
+  plugins: [react(), crx({ manifest })],
+  server: { port: 3000, hmr: { port: 3000 } },
 });
